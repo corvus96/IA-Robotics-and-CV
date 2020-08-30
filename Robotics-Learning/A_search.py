7a035f875109c099e457c7df34331d04b9df0215
Guillermo Raven <45345200+corvus96@users.noreply.github.com>
2020-08-30 13:05:20 -0400

    Create A_search.py

---
+++ Robotics-Learning/A_search.py
@@ -0,0 +1,111 @@
+# -----------
+# User Instructions:
+#
+# Search function should return the expanded grid
+# which shows, for each element, the count when
+# it was expanded or -1 if the element was never expanded.
+# 
+# Also only to understand search() function
+# will print expand, policy, and actions arrays
+#
+# If there is no path from init to goal,
+# the function should return the string 'fail'
+# ----------
+
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+heuristic = [[9, 8, 7, 6, 5, 4],
+             [8, 7, 6, 5, 4, 3],
+             [7, 6, 5, 4, 3, 2],
+             [6, 5, 4, 3, 2, 1],
+             [5, 4, 3, 2, 1, 0]]
+
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def search(grid,init,goal,cost,heuristic):
+    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+
+    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+
+    x = init[0]
+    y = init[1]
+    g = 0
+    h = heuristic[x][y]
+    f = g + h
+    open = [[f, g, h, x, y]]
+
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+    count = 0
+    
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+            return "Fail"
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[3]
+            y = next[4]
+            g = next[1]
+            expand[x][y] = count
+            count += 1
+            
+            if x == goal[0] and y == goal[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            h2 = heuristic[x2][y2]
+                            f2 = g2 + h2
+                            open.append([f2, g2, h2, x2, y2])
+                            closed[x2][y2] = 1
+                            action[x2][y2] = i
+    print("Expand: ")
+    for i in range(len(expand)):
+        print(expand[i])
+    print("\n")
+    
+    print("Action: ")
+    for i in range(len(action)):
+        print(action[i])
+    print("\n")
+    
+    policy = [[" " for col in range(len(grid[0]))] for row in range(len(grid))]
+    
+    x = goal[0]
+    y = goal[1]
+    # Create the path from the goal to the start 
+    policy[x][y] = "*"
+    while x != init[0] or y != init[1]:
+        x2 = x - delta[action[x][y]][0]
+        y2 = y - delta[action[x][y]][1]
+        policy[x2][y2] = delta_name[action[x][y]]
+        x = x2
+        y = y2
+    print("Policy: ")
+    for i in range(len(policy)):
+        print(policy[i])
+    return expand
+    
+# Testing search function
+search(grid,init,goal,cost,heuristic)
