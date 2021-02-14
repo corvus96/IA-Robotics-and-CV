c8ddcf87cb4833a45e324a4fd514ae9000fecc21
Guillermo Raven <45345200+corvus96@users.noreply.github.com>
2021-02-14 17:26:47 -0400

    Lecture 1

---
+++ Learn dynamic Programming/fibonacci.py
@@ -0,0 +1,18 @@
+# memoization
+def fib(n, memo={}):
+    '''Inputs: 
+    n: is the fibonnaci number, for example;
+    n = [1, 2, 3, 4, 5, 6]
+    fib_sec = [1, 1, 2, 3, 5, 8]
+    memo: is a dictionary that their keys are equal to n 
+    an their values are equal to correspond value
+    output: the correspond value to n-esimal fibonnaci number
+    Time complexity = O(n)
+    Space complexity = O(n)                                   
+    '''
+    if(n in memo): return memo[n]
+    if(n <= 2): return 1
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+    return memo[n]
+
+print(fib(110))
